71fe0186fed3d4f5109a088f77c22e3c285b6d58
DC <easetreychen@gmail.com>
2021-05-14 21:16:45 +0800

    add prototype,$alias,etc

--- js/jquery.js
+++ js/jquery.js
@@ -1,85 +1,104 @@
-//2021重写版
-window.jQuery = function (selectorOrArray) {
-    let elements
-    if (typeof selectorOrArray === 'string') {
-        elements = document.querySelectorAll(selectorOrArray)
-    } else if (selectorOrArray instanceof Array) {
-        elements = selectorOrArray
+//2021重制版
+window.$ = window.jQuery = function(selectorOrArrayOrTemplate) {
+    let elements;
+    if (typeof selectorOrArrayOrTemplate === "string") {
+      if (selectorOrArrayOrTemplate[0] === "<") {
+        // 创建 div
+        elements = [createElement(selectorOrArrayOrTemplate)];
+      } else {
+        // 查找 div
+        elements = document.querySelectorAll(selectorOrArrayOrTemplate);
+      }
+    } else if (selectorOrArrayOrTemplate instanceof Array) {
+      elements = selectorOrArrayOrTemplate;
     }
-
-
-    return {
-        oldApi: selectorOrArray.oldApi,
-        find(selector) {
-            let array = []
-            for (let i = 0; i < elements.length; i++) {
-                const elements2 = Array.from(elements[i].querySelectorAll(selector))
-                array = array.concat(elements2)
-                console.log('working in find')
-            }
-            // const newApi = jQuery(array)
-            // return newApi
-
-            array.oldApi = this
-            return jQuery(array)
-        },
-        end() {
-            console.log('end api is working')
-            return this.oldApi
-        }, //新的api ,使其api用在父元素（上一层），而不是子元素
-
-
-        each(fn) {
-            //闭包elements是上面find的，内部调用外部，仍然可用
-            for (let i = 0; i < elements.length; i++) {
-                /**
-                 * 这里用一个fn 意思是each接受传一个函数，并且传入的这个函数还接受一个任意参数
-                 * 例如 elements.each((div)=>{console.log(div)}) 
-                 */
-                fn.call(null, elements[i], i)
-            }
-            return this
-        },
-
-
-        parent() {
-            const array = []
-            this.each((node) => {
-                if (array.indexOf(node.parentNode) === -1) {
-                    array.push(node.parentNode)
-                }
-            })
-            return jQuery(array)
-        },
-
-
-        children() {
-            const array = []
-            this.each((node) => {
-                array.push(...node.children)
-            })
-            return jQuery(array)
-        },
-
-        print() {
-            console.log('print is working')
-            console.log('-- Element is printing now --')
-            for (let i = 0; i < elements.length; i++) {
-                const element = elements[i];
-                console.log(`Element is "${element}" `)
-            }
-            console.log('-- Elements printed --')
-        },
-
-        addClass(className) {
-            //闭包：外部可访问内部变量
-            console.log(1111)
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].classList.add(className)
-
-            }
-            return this //通过return 可以实现链式操作 element.addclass('className2').addclass('className2')
-            //return this === return api
+  
+    function createElement(string) {
+      const container = document.createElement("template");
+      container.innerHTML = string.trim();
+      return container.content.firstChild;
+    }
+    // api 可以操作elements
+    const api = Object.create(jQuery.prototype) // 创建一个对象，这个对象的 __proto__ 为括号里面的东西
+    // const api = {__proto__: jQuery.prototype}
+    Object.assign(api, {
+      elements: elements,
+      oldApi: selectorOrArrayOrTemplate.oldApi
+    })
+    // api.elements = elements
+    // api.oldApi = selectorOrArrayOrTemplate.oldApi
+    return api
+  };
+  
+  jQuery.fn = jQuery.prototype = {
+    constructor: jQuery,
+    jquery: true,
+    get(index) {
+      return this.elements[index];
+    },
+    appendTo(node) {
+      if (node instanceof Element) {
+        this.each(el => node.appendChild(el));
+      } else if (node.jquery === true) {
+        this.each(el => node.get(0).appendChild(el));
+      }
+    },
+    append(children) {
+      if (children instanceof Element) {
+        this.get(0).appendChild(children);
+      } else if (children instanceof HTMLCollection) {
+        for (let i = 0; i < children.length; i++) {
+          this.get(0).appendChild(children[i]);
         }
+      } else if (children.jquery === true) {
+        children.each(node => this.get(0).appendChild(node));
+      }
+    },
+    find(selector) {
+      let array = [];
+      for (let i = 0; i < this.elements.length; i++) {
+        const elements2 = Array.from(this.elements[i].querySelectorAll(selector));
+        array = array.concat(this.elements2);
+      }
+      array.oldApi = this; // this 就是 旧 api
+      return jQuery(array);
+    },
+    each(fn) {
+      for (let i = 0; i < this.elements.length; i++) {
+        fn.call(null, this.elements[i], i);
+      }
+      return this;
+    },
+    parent() {
+      const array = [];
+      this.each(node => {
+        if (array.indexOf(node.parentNode) === -1) {
+          array.push(node.parentNode);
+        }
+      });
+      return jQuery(array);
+    },
+    children() {
+      const array = [];
+      this.each(node => {
+        if (array.indexOf(node.parentNode) === -1) {
+          array.push(...node.children);
+        }
+      });
+      return jQuery(array);
+    },
+    print() {
+      console.log(this.elements);
+    },
+    // 闭包：函数访问外部的变量
+    addClass(className) {
+      for (let i = 0; i < this.elements.length; i++) {
+        const element = this.elements[i];
+        element.classList.add(className);
+      }
+      return this;
+    },
+    end() {
+      return this.oldApi; // this 就是新 api
     }
-}
+  };
